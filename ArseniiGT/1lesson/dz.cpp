b4a481fff5d3435ed02e43c5799ce43576f60d04
Arsenii Rozhkov <59307868+ArseniiGT@users.noreply.github.com>
2023-09-09 17:55:26 +0300

    Update dz.cpp

--- ArseniiGT/1lesson/dz.cpp
+++ ArseniiGT/1lesson/dz.cpp
@@ -1,1 +1,39 @@
 
+#include <iostream>
+#include <string>
+
+class Player
+{
+    std::string name;
+    int lvl;
+    double demage;
+
+public:
+    void input()
+    {
+        std::cin >> name >> lvl >> demage;
+    }
+    void show()
+    {
+        std::cout << name << " " << lvl << " " << demage;
+    }
+};
+int main()
+{
+    Player** ptr = new Player * [3];
+
+    for (int i = 0; i < 3; i++) {
+
+        ptr[i] = new Player;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        ptr[i]->input();
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        ptr[i]->show();
+    }
+    return 0;
+}
