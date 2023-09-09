5b213f4e3a67d8e7efe027b2c62aa88b6e58bd04
Ilya88Weber <39600315+Ilya88Weber@users.noreply.github.com>
2023-09-09 14:05:47 +0400

    task_6.cpp

--- Ilya/Lesson_1/task_6.cpp
+++ Ilya/Lesson_1/task_6.cpp
@@ -1,6 +1,17 @@
 #include <iostream> //input output stream поток ввода и вывода
 
 int main() {
-
+    bool Misha = true;
+    bool Sasha = true;
+    bool Ivan = false;
     
+    bool result = (Misha || (Sasha && Ivan));
+    
+//    if (Misha || (Sasha && Ivan)) {
+//        return 1;
+//    } else {
+//        return 0;
+//    }
+    
+    std::cout << "Результат: " << result << std::endl;
 }
